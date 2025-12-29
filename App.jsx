f6b847bfd9a8dbd35370228c3382a2f28c3b4cf5
ABHISHEK VAISHYA <145659699+ABHISHEK04092004@users.noreply.github.com>
2025-12-29 12:26:07 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+
+import Lottery from "./Lottery";
+import './App.css'
+import {sum} from "./helper";
+
+function App() {
+
+  let winCondition=(ticket)=>{
+    return ticket[0]===0;
+    //  return ticket.every((num)=>num===ticket[0];
+  };
+  
+
+  return(
+    <>
+    <Lottery n={3}  winCondition={winCondition}/>
+    </>
+  );
+
+  
+}
+
+export default App;
